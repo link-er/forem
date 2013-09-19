c9cbbb96215d9bbe6b5bfd2ee0c227a7ff4e98e1
Linara Adylova <adylova.linara.r@gmail.com>
2013-09-19 17:14:10 +0500

    images attributes

--- app/controllers/forem/posts_controller.rb
+++ app/controllers/forem/posts_controller.rb
@@ -46,7 +46,7 @@ module Forem
     private
 
     def post_params
-      params.require(:post).permit(:text, :reply_to_id, image_attributes: [:img_file, :_destroy])
+      params.require(:post).permit(:text, :reply_to_id, images_attributes: [:img_file, :_destroy])
     end
 
     def authorize_reply_for_topic!
